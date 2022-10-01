46b4c6f82f19c601da30063494892f1136eeacd0
Carolina Cruz <caro21_cruz@hotmail.com>
2022-10-01 03:25:37 -0500

    Se agregaron variables y condicion de numero de caracteres permitidos

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Variables
+var letrasmin=["abcdefghijklmnopqrstuvwxyz"];
+var letrasmay=["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numeros=["0123456789"];
+var simbolos=["!#$%&'()*+,-./;<>=?@[]_{}|~"];
 // Código de asignación
 var generateBtn = document.querySelector("#generate");
 
@@ -7,8 +12,16 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
+function generatePassword() {
+  // Numero de caracteres permitidos 
+var num= window.prompt("De cuantos caracteres es tu contraseña");
+  if(num <= 7 || num >= 129) {
+    alert("Tu contraseña debe tener al menos 8 caracteres y no mas de 128");
+    return;
+  } else{
+  alert("Al menos un tipo de los siguientes caracteres debe ser selecciondo");
+  }
+}
 // Agregar oyente de eventos para generar el botón
 generateBtn.addEventListener("click", writePassword);
